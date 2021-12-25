dcbe7d6978c4e8480d3c3b95877a5d1f0e1a7747
blaycoder <onatola4real@gmail.com>
2021-12-25 16:41:01 +0100

    commit on 'master'

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function updateCountdownTime() {
     hours.innerHTML = h < 10 ? '0' + h : h;
     minutes.innerHTML = m < 10 ? '0' + m : m;
     seconds.innerHTML = s < 10 ? '0' + s : s;
+
 }
 
 setInterval(updateCountdownTime, 1000);
